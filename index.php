105ffb96f51506e1c52c07341cf246860bc39887
Katherine Lee <kate.lee168@gmail.com>
2014-01-20 01:10:57 -0500

    initial version of index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<!--Playing with fonts: <link href='http://fonts.googleapis.com/css?family=Gudea|Duru+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">-->
+		<title>Late Meal</title>
+	</head>
+	
+	<body background="images/wood.jpg" bgcolor=#643B0F>
+	<?php include("header.php") ?>
+	<?php include("footer.php") ?>
+	</body>
+	
+</html>
